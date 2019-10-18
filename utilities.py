c724f4aea9eacba36c145ea15c31bbe566ae4209
Anelacu <mmm.lll.2000@gmail.com>
2019-10-18 10:01:29 +0100

    utility file added, some functions initialised

---
+++ utilities.py
@@ -0,0 +1,21 @@
+# Utility funcitons to be used by client.py and server.py
+# Authors - Magdalena Latifa 2398248l and Ivan Nikitin 2292523n
+# Lab group LB10
+import socket
+import sys
+
+
+def send_file(socket, filename):
+    pass
+
+
+def recv_file(socket, filename):
+    pass
+
+
+def send_listing(socket):
+    pass
+
+
+def recv_listing(socket):
+    pass
