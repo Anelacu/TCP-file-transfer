3a8ae55f7c2306c64aa3e1e841ca956cf0cfc90a
Anelacu <mmm.lll.2000@gmail.com>
2019-10-24 19:38:13 +0100

    reorganised exceptions in client.py

--- client.py
+++ client.py
@@ -15,25 +15,35 @@ purpose of client.py:
 """
 import socket
 import sys
-from utilities import send_file,
+from utilities import send_file, recv_file, recv_listing
 
 def exit():
     print('exit')
     sys.exit(1)
 
+class NumberError(Exception):
+    pass
+
 # create a socket
 cli_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get hostname/IP address
-if len(sys.argv) != (5 or 4):
-    print('Invalid number of arguments')
-    exit()
 try:
+    if len(sys.argv) > 5: # too many arguments given
+        raise NumberError
     hostname = str(sys.argv[1])
     port_no = int(sys.argv[2])
     command = str(sys.argv[3])
-    if command == "put" or command = "get":
+    if command == "put" or command == "get":
         file_name = sys.argv[4]
+    elif command == "list": # no argument expected afterwards
+        if len(sys.argv) == 5: # there is an
+            raise NumberError
+    else: # incorrect command inputed
+        raise ValueError
+except (IndexError, NumberError) as e:
+    print('Invalid number of arguments')
+    exit()
 except ValueError:
     print("Invalid arguments")
     exit()
